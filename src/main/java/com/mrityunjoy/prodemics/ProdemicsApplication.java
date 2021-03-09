2a322ddfcab0bb61940be28339f4ab91ffd9696c
coderperfect <mrityunjoynath38@gmail.com>
2021-03-09 18:16:01 +0530

    App Created

---
+++ src/main/java/com/mrityunjoy/prodemics/ProdemicsApplication.java
@@ -0,0 +1,13 @@
+package com.mrityunjoy.prodemics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdemicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdemicsApplication.class, args);
+	}
+
+}
