c55533b085a79605d76e4441a48fa3d8c5369556
Mrityunjoy Nath <mrityunjoynath38@gmail.com>
2026-07-10 14:42:52 +0530

    Added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './layout/footer/footer';
 
 @Component({
     selector: 'app-root',
-    imports: [NavComponent, RouterOutlet],
+    imports: [NavComponent, Footer, RouterOutlet],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
